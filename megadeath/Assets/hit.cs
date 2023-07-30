d8078cade3280ea860a1a264baa93b514501da4a
Griffin <griffinbraewr3@gmail.com>
2023-07-30 14:54:51 -0400

    fist

---
+++ megadeath/Assets/hit.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hit : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+
+    public void attack()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+
+        }
+    }
+}
