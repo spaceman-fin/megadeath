2906168d5585fef365bf8bd024aa7e920c8d3abf
spaceman-fin <finballx@gmail.com>
2023-08-02 10:32:29 -0400

    added pause screen and healthbar to UI

--- megadeath/Assets/Scripts/playerhealth.cs
+++ megadeath/Assets/Scripts/playerhealth.cs
@@ -1,21 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 //goes on the PlayerObj, 
 public class playerhealth : MonoBehaviour
 {
     
     public int health = 100;
+    public Slider healthBar;
+    public TextMeshProUGUI healthText;
 
     void Start()
     {
-        
+        healthBar.minValue = 0f;
+        healthBar.maxValue = health;
     }
 
     
     void Update()
     {
-        
+        healthBar.value = health;
+        healthText.text = health.ToString();
     }
 
     public void OnTriggerEnter(Collider other)
